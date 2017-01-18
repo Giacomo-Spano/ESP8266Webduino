6533ac9e961a20de25a13c4b108a70075547273f
Giacomo <giacomo.spano@libero.it>
2017-01-18 15:10:26 +0100

    summary

--- Command.cpp
+++ Command.cpp
@@ -4,31 +4,17 @@
 #include "DS18S20Sensor.h"
 
 extern Logger logger;
-
-//extern time_t getNtpTime();
 extern const char* statusStr[];
-//char* boolStr[] = { "false", "true" };
+time_t serverTime = 11111111;
 
 int packetcount = 1;
 
 Command* Command::getTimeObject;
-//String Command::HeaterActuatorSubaddress = "01";
 
 
 void digitalClockDisplay(){
-	// digital clock display of the time
-	/*Serial.print(hour());
-	printDigits(minute());
-	printDigits(second());
-	Serial.print(" ");
-	Serial.print(day());
-	Serial.print(" ");
-	Serial.print(month());
-	Serial.print(" ");
-	Serial.print(year());
-	Serial.println();*/
 }
-time_t serverTime =11111111;
+
 
 time_t Command::getNtpTime() {
 	return serverTime;
@@ -42,32 +28,6 @@ time_t xgetNtpTime() {
 
 	String tag = "getNtpTime";
 
-	//logger.println(tag, F("getNtpTime"));
-#ifdef dopo
-	String str = "{";
-	//str += "\"id\":\"" + /*String(shield.id) + */"\"" + "}";
-
-	HttpHelper hplr;
-	String result;
-	boolean res = hplr.post(/*shield.servername*/"http://192.168.1.3",8080/*shield.serverPort*/, "/webduino/time", str, &result);
-	Serial.println("CCC");
-
-	//logger.print(tag, "\n\tanswer = ");
-	//logger.println(tag, result);
-
-	JSON json(result);
-	String resultvalue = json.jsonGetString("result");
-	//logger.print(tag, "\tresult = ");
-	//logger.println(tag, resultvalue);
-
-	if (resultvalue.equalsIgnoreCase("success")) {
-
-		serverTime = json.jsonGetLong("timesec");
-		Serial.println("serverTime=");
-		Serial.println(serverTime);
-		//setSyncProvider(globalGetNTPTime/*getNtpTime*/);
-	}
-#endif
 	return serverTime;
 }
 
@@ -127,7 +87,7 @@ bool Command::sendLog(String log/*, /*int shieldid, String servername, int port*
 
 int Command::registerShield(Shield shield)
 {
-	logger.println(tag, F("REGISTER SHIELD"));
+	logger.println(tag, F(">> registerShield\n"));
 
 
 	String str = "{";
@@ -171,29 +131,29 @@ int Command::registerShield(Shield shield)
 	logger.print(tag, "\tresult = ");
 	logger.print(tag, resultvalue);
 
+	int id = 0;
 	if (resultvalue.equalsIgnoreCase("success")) {
 
-		int id = json.jsonGetInt("id");
-		logger.print(tag, "\n\tid = ");
-		logger.print(tag, String(id));
+		id = json.jsonGetInt("id");
+
 		
 		serverTime = json.jsonGetLong("timesec");
 		getTimeObject = this;
 		//setSyncInterval(60);
 		setSyncProvider(getNtpTime);
-		
-		logger.print(tag, "\n\tid = ");
-		logger.print(tag, String(id));
-		return id;
 	}
-	else {
-		return 0;
-	}
+
+	logger.print(tag, "\n\tid = ");
+	logger.print(tag, String(id));
+
+	logger.println(tag, F("<< registerShield\n"));
+	return id;
+	
 }
 
 int Command::timeSync(/*String servername, int port*/)
 {
-	logger.println(tag, F("timeSync"));
+	logger.println(tag, F(">>timeSync\n"));
 
 	String str = "{";
 	str += "}";
@@ -211,20 +171,24 @@ int Command::timeSync(/*String servername, int port*/)
 	logger.print(tag, "\tresult = ");
 	logger.print(tag, resultvalue);
 
+	time_t srvTime = 0;
 	if (resultvalue.equalsIgnoreCase("success")) {
 		serverTime = json.jsonGetLong("timesec");
+		//srvTime = serverTime;
 		return serverTime;
 	}
-	else {
-		return 0;
-	}
+
+	/*logger.print(tag, "\tsrvTime = ");
+	logger.print(tag, srvTime);
+
+	logger.println(tag, F("<<timeSync\n"));
+	return srvTime;*/
+	return 0;
 }
 
 boolean Command::sendActuatorStatus(HeaterActuator actuator)
 {
-	logger.println(tag, F("SEND ACTUATOR STATUS"));
-
-	//Shield shield;
+	logger.println(tag, F(">>sendActuatorStatus\n"));
 
 	if (Shield::getShieldId() == 0) {
 		logger.println(tag, F("ID NON VALIDO"));
@@ -244,12 +208,13 @@ boolean Command::sendActuatorStatus(HeaterActuator actuator)
 	logger.println(tag, "\n\tanswer = ");
 	logger.println(tag, result);
 
+	logger.println(tag, F("<<sendActuatorStatus\n"));
 	return true;
 }
 
 boolean Command::sendSensorsStatus(Shield shield)
 {
-	logger.println(tag, F("SEND SENSOR STATUS"));
+	logger.println(tag, F(">>sendSensorsStatus"));
 
 	if (Shield::getShieldId() == 0) {
 		logger.print(tag, F("\n\tID NON VALIDO"));
@@ -266,6 +231,8 @@ boolean Command::sendSensorsStatus(Shield shield)
 	bool res = hplr.post(Shield::getServerName(), Shield::getServerPort(), "/webduino/sensor", str, &result);
 	logger.print(tag, "\n\tanswer = ");
 	logger.print(tag, result);
+
+	logger.println(tag, F("<<sendSensorsStatus\n"));
 	return res;
 }
 
